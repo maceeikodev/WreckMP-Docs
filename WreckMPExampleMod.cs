a309f01092328f82dbea51653b44caa345e8552c
Maceeiko (Maceeiko Development) <82950850+maceeikodev@users.noreply.github.com>
2025-06-06 12:36:30 +0200

    Added comments

--- WreckMPExampleMod.cs
+++ WreckMPExampleMod.cs
@@ -13,6 +13,7 @@ public class WreckMPExampleMod : Mod
     public override string Version => "1.0.2";
     public override string Description => "Short demonstration on how to sync a mod for WreckMP multiplayer";
 
+    // List of all spawned cubes
     List<MeshRenderer> cube;
     Transform player;
 
@@ -37,6 +38,7 @@ public class WreckMPExampleMod : Mod
         spawnCubeEvent = new GameEvent("example_spawnCube", ReceiveSpawnCube);
         initialSyncEvent = new GameEvent("example_initSync", ReceiveInitialSync);
 
+        // Host takes care of initial sync
         if (WreckMPGlobals.IsHost)
         {
             WreckMPGlobals.OnMemberReady(SendInitialSync);
@@ -45,7 +47,9 @@ public class WreckMPExampleMod : Mod
 
     void Mod_Update()
     {
+        // Spawn new cube
         if (Input.GetKeyUp(KeyCode.End)) MakeCube(player.position, true);
+        // Randomize color of nearest cube
         if (Input.GetKeyDown(KeyCode.Home) && cube.Count > 0)
         {
             int nearest = 0;
@@ -70,10 +74,12 @@ public class WreckMPExampleMod : Mod
 
         var rb = cube.AddComponent<Rigidbody>();
         rb.mass = 10;
+        // Register rigidbody because it's created after the game loaded
         rb.RegisterRigidbody($"grabbable cube index {this.cube.Count}".GetHashCode());
 
         var col = cube.AddComponent<BoxCollider>();
         col.size = Vector3.one * 0.5f;
+        // Collider only for raycast
         col.isTrigger = true;
 
         cube.MakePickable();
@@ -89,6 +95,8 @@ public class WreckMPExampleMod : Mod
         if (sendEvent)
         {
             using var p = spawnCubeEvent.Writer();
+            // Only spawn position of the cube is required to send,
+            // as it's the only thing that will differ every event
             p.Write(position);
             p.Send();
         }
@@ -99,6 +107,10 @@ public class WreckMPExampleMod : Mod
     void ReceiveSpawnCube(GameEventReader packet)
     {
         var cubePosition = packet.ReadVector3();
+        // Make sure to not send event from its own callback
+        // otherwise you'd create a loop!
+        // With more than 2 players you'd initiate packet
+        // duplication which is an unstoppable death
         MakeCube(cubePosition, false);
     }
 
@@ -107,6 +119,9 @@ public class WreckMPExampleMod : Mod
         var col = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f), 1f);
         cube[index].material.color = col;
 
+        // Write index in order to identify the cube,
+        // and only relevant color channels R,G,B
+        // sending ALPHA channel is useless
         using var writer = colorEvent.Writer();
         writer.Write(index);
         writer.Write(col.r);
@@ -124,16 +139,26 @@ public class WreckMPExampleMod : Mod
             g = packet.ReadSingle(),
             b = packet.ReadSingle()
         };
+        // No need to check the index as the cube list
+        // is synced via the cube spawn events
         cube[cubeIndex].material.color = color;
     }
 
     void SendInitialSync(ulong user)
     {
+        // When a user joins, he needs to know
+        // all cubes that exist, in order to sync up properly
         using var p = initialSyncEvent.Writer();
+        // Since we're writing variable count of
+        // cubes, start with writing the length so we know
+        // how many times to loop when reading
         p.Write(cube.Count);
         for (int i = 0; i < cube.Count; i++)
         {
+            // Write only important info; transform and color
             p.Write(cube[i].transform.position);
+            // Write euler angles instead of quaternion as it's smaller
+            p.Write(cube[i].transform.eulerAngles);
             var col = cube[i].material.color;
             p.Write(col.r);
             p.Write(col.g);
@@ -148,6 +173,7 @@ public class WreckMPExampleMod : Mod
         for (int i = 0; i < count; i++)
         {
             var cube = MakeCube(packet.ReadVector3(), false);
+            cube.transform.eulerAngles = packet.ReadVector3();
             cube.material.color = new Color
             {
                 r = packet.ReadSingle(),
